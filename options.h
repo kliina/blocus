405e44f29b95df6b8cc7e5e95115c0b5e5c146f0
kliina <81854712+kliina@users.noreply.github.com>
2021-04-03 13:18:35 +0200

    Create options.h

---
+++ options.h
@@ -0,0 +1,13 @@
+#ifndef MENU_OPTIONS_H
+#define MENU_OPTIONS_H
+
+
+struct parametres{
+	int grille;
+	int ia;
+	int hard;
+};
+struct parametres menu_options(int i, struct parametres o);
+
+
+#endif
