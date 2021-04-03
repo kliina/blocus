7649fddbab7a8e7dca8f5233605d504a27c6026f
kliina <81854712+kliina@users.noreply.github.com>
2021-04-03 13:17:02 +0200

    Create verification.h

---
+++ verification.h
@@ -0,0 +1,7 @@
+#ifndef VERIFICATION_H
+#define VERIFICATION_H
+
+
+int verification_victoire(int taille_grille, int case_prise[taille_grille][taille_grille], int coord_largeur_couleur, int coord_hauteur_couleur, int num_sprite_orange, int num_sprite_bleu);
+
+#endif
